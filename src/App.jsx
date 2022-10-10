ae481aa43269ff8ce5db370ef4cb790378d7a1d2
Rodrigo06Dev <rodrigouribe061290@gmail.com>
2022-10-10 14:31:21 -0500

    background

--- src/App.jsx
+++ src/App.jsx
@@ -1,115 +1,114 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import './App.css'
-import FormUsers from './components/FormUsers'
-import UserCard from './components/UserCard'
-import { AiOutlineUserAdd } from "react-icons/ai";
-import ModalDelete from './components/ModalDelete'
-const baseURL = 'https://users-crud1.herokuapp.com'
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import FormUsers from "./components/FormUsers";
+import UserCard from "./components/UserCard";
+import { AiOutlineUserAdd,AiFillInstagram,AiOutlineTwitter,AiFillFacebook } from "react-icons/ai";
+import ModalDelete from "./components/ModalDelete";
+const baseURL = "https://users-crud1.herokuapp.com";
 
 function App() {
- 
-  const [users, setUsers] = useState()
-  const [updateUser, setUpdateUser] = useState()
-  const [modal, setModal] = useState(false)
-  const [modalDelete, setModalDelete] = useState(false)
+  const [users, setUsers] = useState();
+  const [updateUser, setUpdateUser] = useState();
+  const [modal, setModal] = useState(false);
+  const [modalDelete, setModalDelete] = useState(false);
 
   useEffect(() => {
-    getAllUsers()
-  }, [])
+    getAllUsers();
+  }, []);
 
   const getAllUsers = () => {
-    const URL = `${baseURL}/users/`
-    axios.get(URL)
-    .then(res => setUsers(res.data))
-    .catch(err => console.log(err))
-}
+    const URL = `${baseURL}/users/`;
+    axios
+      .get(URL)
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  };
 
-const createNewUser = data => {
-    const url = `${baseURL}/users/`
-    axios.post(url, data)
-    .then(res => {
-      console.log(res.data)
-      getAllUsers()
-    })
-    .catch(err => console.log(err))
-}
+  const createNewUser = (data) => {
+    const url = `${baseURL}/users/`;
+    axios
+      .post(url, data)
+      .then((res) => {
+        console.log(res.data);
+        getAllUsers();
+      })
+      .catch((err) => console.log(err));
+  };
 
-const deleteUserById = id => {
-  handleModalDelete()
-  const URL = `${baseURL}/users/${id}/`
-  axios.delete(URL)
-  .then(res => {
-    console.log(res.data)   
-    getAllUsers()
-})
-  .catch(err => console.log(err))
-}
+  const deleteUserById = (id) => {
+    handleModalDelete();
+    const URL = `${baseURL}/users/${id}/`;
+    axios
+      .delete(URL)
+      .then((res) => {
+        console.log(res.data);
+        getAllUsers();
+      })
+      .catch((err) => console.log(err));
+  };
 
-const updateUserById = (id,data) => {
-    const URL = `${baseURL}/users/${id}/`
-    axios.patch(URL, data)
-    .then(res => {
-      console.log(res.data)   
-      getAllUsers()
-    })
-  .catch(err => console.log(err))
-  }
+  const updateUserById = (id, data) => {
+    const URL = `${baseURL}/users/${id}/`;
+    axios
+      .patch(URL, data)
+      .then((res) => {
+        console.log(res.data);
+        getAllUsers();
+      })
+      .catch((err) => console.log(err));
+  };
 
   const handleModal = () => {
-    setModal(!modal)
+    setModal(!modal);
     console.log(modal);
-
-  }
+  };
   const handleModalDelete = () => {
-    setModalDelete(!modalDelete)
+    setModalDelete(!modalDelete);
     console.log(modal);
-
-  }
+  };
 
   return (
     <div className="App">
-    <header className='header'>
-      <h1 className='header__title'>USERS CRUD</h1>
-      <div className='header__btn' onClick={handleModal}><AiOutlineUserAdd/></div>  
-    </header>
-      <section className={`modal ${modal ? 'modal--show' : 'modal--hide'}`}>
+      <header className="header">
+        <h1 className="header__title">USERS CRUD</h1>
+        <div className="header__btn" onClick={handleModal}>
+          <AiOutlineUserAdd />
+        </div>
+      </header>
+      <section className={`modal ${modal ? "modal--show" : "modal--hide"}`}>
         <div className="modal__container">
-          <FormUsers 
-          createNewUser = {createNewUser}
-          updateUser = {updateUser}
-          setUpdateUser = {setUpdateUser}
-          updateUserById = {updateUserById}
-          handleModal = {handleModal}
+          <FormUsers
+            createNewUser={createNewUser}
+            updateUser={updateUser}
+            setUpdateUser={setUpdateUser}
+            updateUserById={updateUserById}
+            handleModal={handleModal}
           />
         </div>
       </section>
-      <section className={`modal ${modalDelete ? 'modal--show' : 'modal--hide'}`}>
+      <section
+        className={`modal ${modalDelete ? "modal--show" : "modal--hide"}`}
+      >
         <div className="modal__container">
-            <ModalDelete
-              handleModalDelete = {handleModalDelete}
-            />
+          <ModalDelete handleModalDelete={handleModalDelete} />
         </div>
       </section>
-     
-        <div className="users-container">
-        {
-        users?.map(user => (          
-          <UserCard 
+
+      <div className="users-container">
+        {users?.map((user) => (
+          <UserCard
             key={user.id}
-            user = {user}
-            deleteUserById = {deleteUserById}
-            handleModal = {handleModal}
-            setUpdateUser = {setUpdateUser}
-            handleModalDelete = {handleModalDelete}
+            user={user}
+            deleteUserById={deleteUserById}
+            handleModal={handleModal}
+            setUpdateUser={setUpdateUser}
+            handleModalDelete={handleModalDelete}
           />
-        ))
-      }      
-        </div>
+        ))}
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
